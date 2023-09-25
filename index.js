25c3f814ef5b5f97c35f54872cb1ccde59299b8f
Mohd Faizan <kfaizan0496@gmail.com>
2023-09-25 23:08:08 +0530

    Intialize the npm init and install necessary packages

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+
+const port=8000;
+
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log("Error in server running")
+    }
+    console.log("Server is up and running")
+
+})

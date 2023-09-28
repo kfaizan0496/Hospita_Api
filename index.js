238a8fa27302d051f50482ee0c219981b8065ee7
Mohd Faizan <kfaizan0496@gmail.com>
2023-09-28 15:58:18 +0530

    check the index.js file is it working or not

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express=require('express');
 const app=express();
 
+
 const port=8000;
 
 
 
 
-app.listen(port,function(err){
-    if(err){
-        console.log("Error in server running")
-    }
-    console.log("Server is up and running")
 
-})
+    app.listen(port, function (error) {
+        if (error) {
+            console.log(`Error in running the Server. Error is : ${error}`);
+            return;
+        }
+        console.log(`Server is up and running on the port: ${port}`);
+    })

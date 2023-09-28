2b1346d6b756df19f701ec9ca191f3cfdfdf5b15
Mohd Faizan <kfaizan0496@gmail.com>
2023-09-28 16:07:07 +0530

    Create Schema for reports

---
+++ models/reports.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+// Create Schema for Report of the Patients
+
+const reportSchema = new mongoose.Schema({
+  createdByDoctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  status: {
+    type:String,
+    require:true,
+    // enum is used because  only specific values are allowed
+    enum:['Negative', 'Travelled-Quarantine', 'Symptoms-Quarantine',
+        'Positive-Admit']
+  },
+  date:{
+    type: Date,
+    required: true,
+  }
+}, {
+  timestamps: true
+}
+)
+//  Declare models in the database
+const Reports = mongoose.model('Reports', reportSchema);
+
+module.exports = Reports;

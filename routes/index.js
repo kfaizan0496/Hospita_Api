759b7685cfd82b2928c6cffb25b5f36ee3e1c120
Mohd Faizan <kfaizan0496@gmail.com>
2023-09-28 16:17:35 +0530

    Create index.js file for main file of routing

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+router.use('/doctors', require('./doctors'))
+router.use('/patients', require('./patients'))
+
+
+
+module.exports = router;

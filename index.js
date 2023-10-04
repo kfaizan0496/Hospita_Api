35e7af90765f651cb9dce1b8129070622ae7f962
Mohd Faizan <kfaizan0496@gmail.com>
2023-10-04 10:46:39 +0530

    adding confedentiality using .env files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express=require('express');
+const dotenv=require('dotenv')
+dotenv.config();
 const app=express();
 
 
-const port=8000;
+const port=process.env.PORT ||8000;
 // used database
 const db = require('./config/mongoose')
 
